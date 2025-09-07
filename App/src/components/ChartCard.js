ddba772893aae1a2a8105abb24b87c3def29d671
IbrokhimN <161945489+IbrokhimN@users.noreply.github.com>
2025-09-08 00:25:37 +0500

    Update ChartCard.js

--- App/src/components/ChartCard.js
+++ App/src/components/ChartCard.js
@@ -1,1 +1,10 @@
 
+export function ChartCard(label, value, sub) {
+  return `
+    <div class="card">
+      <div class="label">${label}</div>
+      <div class="value">${value}</div>
+      <div class="small">${sub}</div>
+    </div>
+  `;
+}
