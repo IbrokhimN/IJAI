eb1682df013b02a33a5d82fab19c33dd0a7c6ea3
IbrokhimN <161945489+IbrokhimN@users.noreply.github.com>
2025-09-11 14:38:42 +0500

    Update magnificent.asm

--- dataset/magnificent.asm
+++ dataset/magnificent.asm
@@ -1,22 +1,62 @@
 section .data
-    heart db " ▄▄▄     ▄▄▄ ▄▄▄▄▄▄▄ ▄▄▄ ",10
-          db "█   █   █   █       █   █",10
-          db "█   █   █   █   ▄   █   █",10
-          db "█   █▄  █   █  █▄█  █   █",10
-          db "█   █ █▄█   █       █   █",10
-          db "█   █       █   ▄   █   █",10
-          db "█▄▄▄█▄▄▄▄▄▄▄█▄▄█ █▄▄█▄▄▄█",10
+    red     db 27,"[31m",0
+    reset   db 27,"[0m",0
+    clear   db 27,"[2J",27,"[H",0
+
+    heart db "   *****     *****   ",10
+          db "  *******   *******  ",10
+          db " ********* ********* ",10
+          db " ******************* ",10
+          db "  *****************  ",10
+          db "   ***************   ",10
+          db "    *************    ",10
+          db "     ***********     ",10
+          db "      *********      ",10
+          db "       *******       ",10
+          db "        *****        ",10
+          db "         ***         ",10
+          db "          *          ",10
           db 0
     len equ $ - heart
 
 section .text
     global _start
+
 _start:
+.loop:
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, clear
+    mov rdx, 7
+    syscall
+
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, red
+    mov rdx, 5
+    syscall
+
     mov rax, 1
     mov rdi, 1
     mov rsi, heart
     mov rdx, len
     syscall
-    mov rax, 60
-    xor rdi, rdi
+
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, reset
+    mov rdx, 4
     syscall
+
+    mov rax, 35
+    lea rdi, [rel ts1]
+    xor rsi, rsi
+    syscall
+
+    jmp .loop
+
+section .bss
+    align 8
+ts1:
+    dq 0
+    dq 200000000
