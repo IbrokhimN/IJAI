eb4bdb7acaa657ca94bb0e0e8ffec2c0a6c1c8b8
IbrokhimN <161945489+IbrokhimN@users.noreply.github.com>
2025-09-11 14:40:04 +0500

    Update magnificent.asm

--- dataset/magnificent.asm
+++ dataset/magnificent.asm
@@ -1,9 +1,11 @@
 section .data
-    red     db 27,"[31m",0
-    bold    db 27,"[1m",0
-    reset   db 27,"[0m",0
     clear   db 27,"[2J",27,"[H",0
-    center  db 27,"[10;20H",0
+    center  db 27,"[10;30H",0
+
+    color1  db 27,"[31;1m",0   ; ярко-красный (bold)
+    color2  db 27,"[31m",0     ; обычный красный
+    color3  db 27,"[90m",0     ; серый (затухание)
+    reset   db 27,"[0m",0
 
     heart1 db "   ***     ***   ",10
            db " *****   *****  ",10
@@ -40,95 +42,118 @@ section .text
 
 _start:
 .loop:
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, clear
-    mov rdx, 7
+    ; ярко-красный + маленькое сердце
+    mov rax,1
+    mov rdi,1
+    mov rsi,clear
+    mov rdx,7
     syscall
 
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, center
-    mov rdx, 7
+    mov rax,1
+    mov rdi,1
+    mov rsi,center
+    mov rdx,7
     syscall
 
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, red
-    mov rdx, 5
+    mov rax,1
+    mov rdi,1
+    mov rsi,color1
+    mov rdx,6
     syscall
 
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, bold
-    mov rdx, 4
+    mov rax,1
+    mov rdi,1
+    mov rsi,heart1
+    mov rdx,len1
     syscall
 
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, heart1
-    mov rdx, len1
+    mov rax,1
+    mov rdi,1
+    mov rsi,reset
+    mov rdx,4
     syscall
 
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, reset
-    mov rdx, 4
+    mov rax,35
+    lea rdi,[rel ts1]
+    xor rsi,rsi
     syscall
 
-    mov rax, 35
-    lea rdi, [rel ts1]
-    xor rsi, rsi
+    ; обычный красный + большое сердце
+    mov rax,1
+    mov rdi,1
+    mov rsi,clear
+    mov rdx,7
     syscall
 
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, clear
-    mov rdx, 7
+    mov rax,1
+    mov rdi,1
+    mov rsi,center
+    mov rdx,7
     syscall
 
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, center
-    mov rdx, 7
+    mov rax,1
+    mov rdi,1
+    mov rsi,color2
+    mov rdx,5
     syscall
 
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, red
-    mov rdx, 5
+    mov rax,1
+    mov rdi,1
+    mov rsi,heart2
+    mov rdx,len2
     syscall
 
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, bold
-    mov rdx, 4
+    mov rax,1
+    mov rdi,1
+    mov rsi,reset
+    mov rdx,4
     syscall
 
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, heart2
-    mov rdx, len2
+    mov rax,35
+    lea rdi,[rel ts2]
+    xor rsi,rsi
     syscall
 
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, reset
-    mov rdx, 4
+    ; затухающее серое + маленькое сердце
+    mov rax,1
+    mov rdi,1
+    mov rsi,clear
+    mov rdx,7
     syscall
 
-    mov rax, 35
-    lea rdi, [rel ts2]
-    xor rsi, rsi
+    mov rax,1
+    mov rdi,1
+    mov rsi,center
+    mov rdx,7
+    syscall
+
+    mov rax,1
+    mov rdi,1
+    mov rsi,color3
+    mov rdx,5
+    syscall
+
+    mov rax,1
+    mov rdi,1
+    mov rsi,heart1
+    mov rdx,len1
+    syscall
+
+    mov rax,1
+    mov rdi,1
+    mov rsi,reset
+    mov rdx,4
+    syscall
+
+    mov rax,35
+    lea rdi,[rel ts3]
+    xor rsi,rsi
     syscall
 
     jmp .loop
 
 section .bss
     align 8
-ts1:
-    dq 0
-    dq 120000000
-ts2:
-    dq 0
-    dq 120000000
+ts1: dq 0,150000000
+ts2: dq 0,150000000
+ts3: dq 0,150000000
