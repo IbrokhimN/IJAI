e3001530b2d06bc409deaae117ed712568bbb994
IbrokhimN <161945489+IbrokhimN@users.noreply.github.com>
2025-09-11 14:39:03 +0500

    Update magnificent.asm

--- dataset/magnificent.asm
+++ dataset/magnificent.asm
@@ -1,23 +1,38 @@
 section .data
     red     db 27,"[31m",0
+    bold    db 27,"[1m",0
     reset   db 27,"[0m",0
     clear   db 27,"[2J",27,"[H",0
 
-    heart db "   *****     *****   ",10
-          db "  *******   *******  ",10
-          db " ********* ********* ",10
-          db " ******************* ",10
-          db "  *****************  ",10
-          db "   ***************   ",10
-          db "    *************    ",10
-          db "     ***********     ",10
-          db "      *********      ",10
-          db "       *******       ",10
-          db "        *****        ",10
-          db "         ***         ",10
-          db "          *          ",10
-          db 0
-    len equ $ - heart
+    heart1 db "   ***     ***   ",10
+           db " *****   *****  ",10
+           db "****************",10
+           db " ************** ",10
+           db "  ************  ",10
+           db "   **********   ",10
+           db "    ********    ",10
+           db "     ******     ",10
+           db "      ****      ",10
+           db "       **       ",10
+           db 0
+    len1 equ $ - heart1
+
+    heart2 db "   *****     *****   ",10
+           db "  *******   *******  ",10
+           db " ******************* ",10
+           db "*********************",10
+           db " ******************* ",10
+           db "  *****************  ",10
+           db "   ***************   ",10
+           db "    *************    ",10
+           db "     ***********     ",10
+           db "      *********      ",10
+           db "       *******       ",10
+           db "        *****        ",10
+           db "         ***         ",10
+           db "          *          ",10
+           db 0
+    len2 equ $ - heart2
 
 section .text
     global _start
@@ -38,8 +53,14 @@ _start:
 
     mov rax, 1
     mov rdi, 1
-    mov rsi, heart
-    mov rdx, len
+    mov rsi, bold
+    mov rdx, 4
+    syscall
+
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, heart1
+    mov rdx, len1
     syscall
 
     mov rax, 1
@@ -53,10 +74,48 @@ _start:
     xor rsi, rsi
     syscall
 
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, clear
+    mov rdx, 7
+    syscall
+
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, red
+    mov rdx, 5
+    syscall
+
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, bold
+    mov rdx, 4
+    syscall
+
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, heart2
+    mov rdx, len2
+    syscall
+
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, reset
+    mov rdx, 4
+    syscall
+
+    mov rax, 35
+    lea rdi, [rel ts2]
+    xor rsi, rsi
+    syscall
+
     jmp .loop
 
 section .bss
     align 8
 ts1:
     dq 0
-    dq 200000000
+    dq 150000000
+ts2:
+    dq 0
+    dq 150000000
